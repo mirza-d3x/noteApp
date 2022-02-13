209316ea9ec666781411e04124c9a5686ccb09e6
Mohammed Mirsa <mohammedmirsa010@gmail.com>
2022-02-13 11:32:49 +0530

    note item finished now screen add item screen

--- lib/screens/screen_add_notes.dart
+++ lib/screens/screen_add_notes.dart
@@ -1,12 +1,46 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
+enum ActionType {
+  addNote,
+  editNote,
+}
+
 class ScreenAddNote extends StatelessWidget {
-  const ScreenAddNote({ Key? key }) : super(key: key);
+  final ActionType type;
+  final String id;
+  const ScreenAddNote({
+    Key? key,
+    required this.type,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+  Widget get saveButton => TextButton.icon(
+        onPressed: () {
+          switch (type) {
+            case ActionType.addNote:
+              // Add Note
+              break;
+            case ActionType.editNote:
+              // Edit Note
+              break;
+          }
+        },
+        icon: const Icon(
+          Icons.save,
+        ),
+        label: const Text(
+          'Save',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold();
   }
 }
--- lib/widget/note_item.dart
+++ lib/widget/note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app_sample/screens/screen_add_notes.dart';
 
 class NoteItem extends StatelessWidget {
   final String id;
@@ -14,7 +15,61 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){},
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => ScreenAddNote(
+              type: ActionType.editNote,
+              id: id,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.grey,
+          ),
+        ),
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                ),
+                Text(
+                  content,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
